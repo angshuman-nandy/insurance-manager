8f38ac11c5a6e82812b992076752252fb0a5a31b
Angshuman Nandy <angshuman.nandy@gmail.com>
2017-07-12 19:21:04 +0530

    revert back gem and db.yml

--- Gemfile
+++ Gemfile
@@ -11,8 +11,7 @@ gem 'rails', '~> 5.0.2'
 # Use mysql as the database for Active Record
 gem 'record_tag_helper', '~> 1.0'
 # Use Puma as the app server
-gem 'pg'
-gem 'rails_12factor'
+gem 'mysql2', '>= 0.3.18', '< 0.5'
 gem 'puma', '~> 3.0'
 gem 'bcrypt', '~> 3.1', '>= 3.1.11'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
--- db/schema.rb
+++ db/schema.rb
@@ -12,23 +12,23 @@
 
 ActiveRecord::Schema.define(version: 20170606154031) do
 
-  create_table "comments", force: :cascade do |t|
-    t.text     "body"
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "body",       limit: 65535
     t.integer  "user"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.integer  "policy_id"
-    t.index ["policy_id"], name: "index_comments_on_policy_id"
+    t.index ["policy_id"], name: "index_comments_on_policy_id", using: :btree
   end
 
-  create_table "companies", force: :cascade do |t|
+  create_table "companies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
-  create_table "details", id: false, force: :cascade do |t|
+  create_table "details", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",          null: false
     t.string   "first_name"
     t.string   "last_name"
@@ -38,54 +38,61 @@ ActiveRecord::Schema.define(version: 20170606154031) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "user_id"
-    t.index ["user_id"], name: "index_details_on_user_id"
+    t.index ["user_id"], name: "index_details_on_user_id", using: :btree
   end
 
-  create_table "policies", force: :cascade do |t|
+  create_table "policies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "policy_type"
     t.string   "description"
-    t.decimal  "sum_insured"
-    t.decimal  "premium_amount"
-    t.decimal  "commission"
+    t.decimal  "sum_insured",    precision: 10
+    t.decimal  "premium_amount", precision: 10
+    t.decimal  "commission",     precision: 10
     t.date     "purchase_date"
     t.date     "mature_date"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.integer  "user_id"
     t.integer  "company_id"
     t.integer  "poltype_id"
-    t.index ["company_id"], name: "index_policies_on_company_id"
-    t.index ["poltype_id"], name: "index_policies_on_poltype_id"
-    t.index ["user_id"], name: "index_policies_on_user_id"
+    t.index ["company_id"], name: "index_policies_on_company_id", using: :btree
+    t.index ["poltype_id"], name: "index_policies_on_poltype_id", using: :btree
+    t.index ["user_id"], name: "index_policies_on_user_id", using: :btree
   end
 
-  create_table "poltypes", force: :cascade do |t|
+  create_table "poltypes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.integer  "company_id"
     t.integer  "duration"
-    t.decimal  "premium_amount"
-    t.decimal  "sum_insured"
-    t.index ["company_id"], name: "index_poltypes_on_company_id"
+    t.decimal  "premium_amount", precision: 10
+    t.decimal  "sum_insured",    precision: 10
+    t.index ["company_id"], name: "index_poltypes_on_company_id", using: :btree
   end
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
-    t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "text",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
     t.integer  "user_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",           default: "", null: false
     t.string   "password_digest", default: "", null: false
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "policies"
+  add_foreign_key "details", "users"
+  add_foreign_key "policies", "companies"
+  add_foreign_key "policies", "poltypes"
+  add_foreign_key "policies", "users"
+  add_foreign_key "poltypes", "companies"
+  add_foreign_key "posts", "users"
 end
